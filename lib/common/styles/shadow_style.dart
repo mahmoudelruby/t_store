52f624689586fbc8ab950ed316e2e15a9ce01dc4
Mahmoud Elruby <elruby71@gmail.com>
2024-01-05 00:42:09 +0200

    add home screen design ui

---
+++ lib/common/styles/shadow_style.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/utils/constants/colors.dart';
+
+class ShadowStyle{
+static final verticalProductShadow=BoxShadow(
+  color: TColors.darkerGrey.withOpacity(.1),
+  blurRadius: 50,
+  spreadRadius: 7,
+  offset: const Offset(0,2)
+
+);
+static final horizontalProductShadow=BoxShadow(
+    color: TColors.darkerGrey.withOpacity(.1),
+    blurRadius: 50,
+    spreadRadius: 7,
+    offset: const Offset(0,2)
+
+);
+
+}
---
+++ lib/common/widgets/icons/cicular_icon.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/helpers/helper_func.dart';
+
+class CircularIcon extends StatelessWidget {
+  const CircularIcon({
+    super.key,
+    this.width,
+    this.height,
+    this.size = TSizes.lg,
+    required this.icon,
+    this.color,
+    this.backgroundColor,
+    this.onPressed,
+  });
+  final double? width, height, size;
+  final IconData icon;
+  final Color? color;
+  final Color? backgroundColor;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    final darkMode = THelperFunctions.isDarkMode(context);
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: backgroundColor != null
+              ? backgroundColor!
+              : darkMode
+                  ? TColors.black.withOpacity(.9)
+                  : TColors.white.withOpacity(.9)),
+      child: IconButton(
+        onPressed: onPressed,
+        icon: Icon(icon,color: color,size: size,),
+      ),
+    );
+  }
+}
